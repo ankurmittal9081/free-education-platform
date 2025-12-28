86e5cbc876945366a65c69f5cf756653b1a93bcb
Ankur Mittal <ankurmittal9081@gmail.com>
2025-12-28 22:58:13 +0530

    Updated firebase config to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhz0Jvy2oHPaSHLoovwBgKeQGwiDtPurc",
-  authDomain: "micro-harbor-455116-v2.firebaseapp.com",
-  projectId: "micro-harbor-455116-v2",
-  storageBucket: "micro-harbor-455116-v2.firebasestorage.app",
-  messagingSenderId: "596871723352",
-  appId: "1:596871723352:web:fa3317c9474dc244004d2d",
-  measurementId: "G-1NDECLESDR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
